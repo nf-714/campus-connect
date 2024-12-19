db26d39bc9ce903bd2b002da66d01cce43c68960
nf-714 <nfuad714@gmail.com>
2024-12-19 18:32:46 +0600

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Toaster } from "@/components/ui/toaster";
+import ReactQueryProvider from "@/providers/react-query.provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        {" "}
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <Toaster />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,38 @@
+import { auth, signOut } from "@/auth/auth";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth();
+
+  const RenderBasedOnSessionAvailability = () => {
+    if (session) {
+      return (
+        <div>
+          <p>Session:</p>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+          <form
+            action={async () => {
+              "use server";
+              await signOut();
+            }}
+          >
+            <Button type="submit">Sign Out</Button>
+          </form>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <p>Not signed in</p>
+        <Link href="/sign-in">
+          {" "}
+          <Button>Click to login</Button>
+        </Link>
+      </div>
+    );
+  };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -22,7 +54,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-
+        <RenderBasedOnSessionAvailability />
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
